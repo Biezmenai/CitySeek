53ba03a7611fa3843bbffee280c6cfb8a9d4c708
Lukas <lukas.kucinskas@ktu.edu>
2016-03-17 19:42:17 +0200

    Luko pc prijungimas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 - DariausPc</div>
+                <div class="title">Laravel 5 - LukoPc</div>
             </div>
         </div>
     </body>
