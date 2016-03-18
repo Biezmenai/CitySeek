8f818c731dd5befe90d984cf8277ec7a7a600d57
linas <levanaslinas@gmail.com>
2016-03-18 11:46:53 +0200

    Lino pc prisijungimas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5 - LukoPc</div>
+                <div class="title">Laravel 5 - LinoPc</div>
             </div>
         </div>
     </body>
