78a4052846a53dc4f5642977785dbd5d39271d6e
linas <levanaslinas@gmail.com>
2016-05-24 15:51:31 +0300

    css added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,53 +1,90 @@
 <!DOCTYPE html>
+
+
 <html>
-    <head>
-        <title>Laravel</title>
+<head>
+    <title>Visualize by TEMPLATED</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+</head>
+<body>
 
-        <link href="//fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+<!-- Wrapper -->
+<div id="wrapper">
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+    <!-- Header -->
+    <header id="header">
+        <span class="avatar"><img src="{{ Auth::user()->avatar }}" alt="" /></span>
+        <ul class="icons">
+            <li><a href="#" class="icon style2 fa-twitter"><span class="label">Twitter</span></a></li>
+            <li><a href="#" class="icon style2 fa-facebook"><span class="label">Facebook</span></a></li>
+            <li><a href="#" class="icon style2 fa-instagram"><span class="label">Instagram</span></a></li>
+            <li><a href="#" class="icon style2 fa-500px"><span class="label">500px</span></a></li>
+            <li><a href="#" class="icon style2 fa-envelope-o"><span class="label">Email</span></a></li>
+        </ul>
+    </header>
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
+    <!-- Main -->
+    <section id="main">
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-                    You are Logged In
-                <div>
-                   <h4>Your name is  {{ Auth::user()->name }} </h4>
-                   <h4>Your email is {{ Auth::user()->email }} </h4>
-                   <img src="{{ Auth::user()->avatar }}" height="100" width="100" /><br>
-                    <a class="btn btn-info" href="auth/logout" role="button">Logout</a>
-                </div>
+        <!-- Thumbnails -->
+        <section class="thumbnails">
+            <div>
+                <a href="images/fulls/01.jpg">
+                    <img src="images/thumbs/01.jpg" alt="" />
+                    <h3>Lorem ipsum dolor sit amet</h3>
+                </a>
+                <a href="images/fulls/02.jpg">
+                    <img src="images/thumbs/02.jpg" alt="" />
+                    <h3>Lorem ipsum dolor sit amet</h3>
+                </a>
             </div>
+            <div>
+                <a href="images/fulls/03.jpg">
+                    <img src="images/thumbs/03.jpg" alt="" />
+                    <h3>Lorem ipsum dolor sit amet</h3>
+                </a>
+                <a href="images/fulls/04.jpg">
+                    <img src="images/thumbs/04.jpg" alt="" />
+                    <h3>Lorem ipsum dolor sit amet</h3>
+                </a>
+                <a href="images/fulls/05.jpg">
+                    <img src="images/thumbs/05.jpg" alt="" />
+                    <h3>Lorem ipsum dolor sit amet</h3>
+                </a>
+            </div>
+            <div>
+                <a href="images/fulls/06.jpg">
+                    <img src="images/thumbs/06.jpg" alt="" />
+                    <h3>Lorem ipsum dolor sit amet</h3>
+                </a>
+                <a href="images/fulls/07.jpg">
+                    <img src="images/thumbs/07.jpg" alt="" />
+                    <h3>Lorem ipsum dolor sit amet</h3>
+                </a>
+            </div>
+        </section>
+        <div>
+            <h4>Your name is  {{ Auth::user()->name }} </h4>
+            <h4>Your email is {{ Auth::user()->email }} </h4>
+            <img src="{{ Auth::user()->avatar }}" height="100" width="100" /><br>
+            <a class="btn btn-info" href="auth/logout" role="button">Logout</a>
         </div>
-    </body>
+    </section>
+
+    <!-- Footer -->
+    <footer id="footer">
+        <p>&copy; Untitled. All rights reserved. Design: <a href="http://templated.co">TEMPLATED</a>. Demo Images: <a href="http://unsplash.com">Unsplash</a>.</p>
+    </footer>
+
+</div>
+
+<!-- Scripts -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/jquery.poptrox.min.js"></script>
+<script src="assets/js/skel.min.js"></script>
+<script src="assets/js/main.js"></script>
+
+</body>
 </html>
