191cdbb36800e8a2a14d58f1e5eb60992f1575ea
Kristupas <kristupas.vaitkus@ktu.edu>
2016-05-25 00:56:45 +0300

    Fixes

--- resources/views/kontaktai.blade.php
+++ resources/views/kontaktai.blade.php
@@ -10,10 +10,27 @@
 </head>
 <body>
 
-<h1>Susisiekite su mumis</h1>
+
+
+
+<h1 align="center"> Susisiekite su mumis</h1>
+<div align="center">
+    <ul class="icons" align="center">
+        <li><a href="https://twitter.com/CitySeekKaunas" target="_blank" class="icon style2 fa-twitter"><span class="label">Twitter</span></a></li>
+        <li><a href="https://www.facebook.com/cityseekkaunas/" target="_blank" class="icon style2 fa-facebook"><span class="label">Facebook</span></a></li>
+        <li><a href="https://www.instagram.com/cityseekkaunas/?hl=en" target="_blank" class="icon style2 fa-instagram"><span class="label">Instagram</span></a></li>
+        <li><a href="mailto:cityseekinfo@gmail.com" class="icon style2 fa-envelope-o"><span class="label">Email</span></a></li>
+    </ul>
+</div>
 
 </body>
 
+<h2 align="center">
+    <a  href="/home">
+        <img  src="images/thumbs/home3.png" alt="" />
+        <h3>Grįžti į pradinį puslapį</h3>
+    </a>
+</h2>
 <!-- Footer -->
 <footer id="footer">
     <p>&copy; CitySeek </a>.</p>
