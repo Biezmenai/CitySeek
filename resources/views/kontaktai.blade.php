75228a84f4c0d36fc12caade615c7baada55c107
linas <levanaslinas@gmail.com>
2016-05-25 01:34:23 +0300

    Get

--- resources/views/kontaktai.blade.php
+++ resources/views/kontaktai.blade.php
@@ -33,8 +33,10 @@
     <!-- Main -->
     <section id="main">
         <h1>Susisiekite su mumis</h1>
-        Su mumis galite susiekti paciuldami man byrka
-        <h1>Navigacija</h1>
+        Su mumis galite susiekti šiuo el. pašto adresu. Pažadame atsakyti į jūsų užklausą per 24 valandas!<br><br>
+        Elektroninis paštas: <strong>cityseekinfo@gmail.com<strong><br>
+
+        <br><h1>Navigacija</h1>
         <!-- Thumbnails -->
         <section class="thumbnails">
             <div>
