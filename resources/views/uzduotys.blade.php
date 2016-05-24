f4c4fda5dbf4b15f87e88e664fc79a92218d1e62
Darius <darius.kraujutaitis@ktu.edu>
2016-05-24 22:24:14 +0300

    Css uzduotys

--- resources/views/uzduotys.blade.php
+++ resources/views/uzduotys.blade.php
@@ -2,7 +2,14 @@
 
 
 <html>
+<head>
+    <title>Visualize by TEMPLATED</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+</head>
 <body>
+
 <h1>Davai uzduotele gal ikelsi kokia?</h1>
 </body>
 </html>
