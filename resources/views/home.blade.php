50d9af2f066e1b838646a17dc472267506a3ea58
linas <levanaslinas@gmail.com>
2016-05-24 19:33:55 +0300

    thumbnails

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -38,11 +38,11 @@
         <section class="thumbnails">
             <div>
                 <a href="https://twitter.com/CitySeekKaunas" target="_blank">
-                    <img src="images/thumbs/01.jpg" alt="" />
-                    <h3>Lorem ipsum dolor sit amet</h3>
+                    <img src="images/thumbs/listing.png" alt="" />
+                    <h3>Turnyrinė lentelė</h3>
                 </a>
                 <a href="images/fulls/02.jpg">
-                    <img src="images/thumbs/02.jpg" alt="" />
+                    <img src="images/thumbs/tasks.png" alt="" />
                     <h3>Lorem ipsum dolor sit amet</h3>
                 </a>
             </div>
