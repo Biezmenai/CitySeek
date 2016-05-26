9954718265cdd384472814be94b3fd0a0716ac66
linas <levanaslinas@gmail.com>
2016-05-26 21:09:33 +0300

    PABAIGTA

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -19,6 +19,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/', array('before' => 'auth'));
+
 Route::get('auth/facebook', 'Auth\AuthController@redirectToProvider');
 Route::post('ideti', 'UploadController@ideti');
 Route::post('prideti_uzduoti', 'AdminController@prideti_uzduoti');
