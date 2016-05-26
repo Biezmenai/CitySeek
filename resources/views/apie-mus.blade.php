d9c4dc46d4fff9964aa48b96831939c4159c8085
linas <levanaslinas@gmail.com>
2016-05-26 19:43:29 +0300

    Get

--- resources/views/apie-mus.blade.php
+++ resources/views/apie-mus.blade.php
@@ -39,7 +39,7 @@
                     <td> </td>
                 </tr>
                 @foreach( $users as $key => $h)
-		    <?php  { ?>
+		    <?php if ($h->accesslevel > 0) { ?>
                     <tr>
                         <td>{{ $h->name }}</td> <td>{{"IF-4/9"}}</td> <td><img src="{{ $h->avatar }}" alt=""/> </td>
                     </tr>
